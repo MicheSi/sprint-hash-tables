c59bf9279cc878bb02cd5a3850807afe7a89759f
mlazzell17@gmail.com <MicheSi>
2020-06-06 09:50:48 -0700

    completed ex2 and tests passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,27 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    flights = {}
+    route = []
+
+    # create key & value for each ticket
+    for i in range(length):
+        key = tickets[i].source
+        value = tickets[i].destination
+
+        flights[key] = value
+
+    # if key is NONE, flight is start of route
+    key = flights['NONE']
+
+    # if value of key is not NONE, add flights to route
+    while key != 'NONE':
+        route.append(key)
+
+        # set key to new destination
+        key = flights[key]
+
+    # value of key is NONE, it's the last destination
+    route.append(key)
 
     return route
