a3471cf629eee42dcb8f7851f12f67fcc10c8eb7
mlazzell17@gmail.com <MicheSi>
2020-06-06 09:40:38 -0700

    completed ex1 and tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,29 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight = {}
 
+    for i in range(length):
+        cur = weights[i]
+        # if current weight already in dict, add it to value of key
+        if cur in weight:
+            weight[cur].append(i)
+        # if current weight not in dict, current = current index
+        else:
+            weight[cur] = [i]
+    
+    for key in weight:
+        # subtract current key(weight) from limit to find target weight
+        target = limit - key
+        # if target weight in dict
+        if target in weight:
+            # if target weight has a duplicate
+            if len(weight[target]) != 1:
+                # return tuple of indices of duplicates
+                return tuple(sorted(weight[target], reverse=True))
+
+            else:
+                # return tuple with target weight and key
+                return tuple(sorted((weight[target][0], weight[key][0]), reverse=True))
+    # if key does not exist, return None
     return None
